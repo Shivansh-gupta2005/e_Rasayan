a7cbf48561af9ef69e800f50b004bca6de302854
Shivansh-gupta2005 <147073873+Shivansh-gupta2005@users.noreply.github.com>
2024-07-14 17:36:02 +0530

    Add changes

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 // Google Apps Script code (Code.gs)
 
 function doPost(e) {
-    const sheet = SpreadsheetApp.openById('Y1tvQtrz3XICVWuTksU-JnQzbe3Gy0fBbfF0T8YRKsc_A').getActiveSheet(); // Replace with your Google Sheet ID
+    const sheet = SpreadsheetApp.openById('1tvQtrz3XICVWuTksU-JnQzbe3Gy0fBbfF0T8YRKsc_A').getActiveSheet(); // Replace with your Google Sheet ID
     const newRow = sheet.getLastRow() + 1;
     const rowData = [];
     
