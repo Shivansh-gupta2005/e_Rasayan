5961fc161465743f0061d57b2b2f1e3accb1d54d
Shivansh-gupta2005 <147073873+Shivansh-gupta2005@users.noreply.github.com>
2024-07-14 17:20:35 +0530

    Add changes

---
+++ Code.js
@@ -0,0 +1,17 @@
+// Google Apps Script code (Code.gs)
+
+function doPost(e) {
+    const sheet = SpreadsheetApp.openById('Y1tvQtrz3XICVWuTksU-JnQzbe3Gy0fBbfF0T8YRKsc_A').getActiveSheet(); // Replace with your Google Sheet ID
+    const newRow = sheet.getLastRow() + 1;
+    const rowData = [];
+    
+    rowData.push(new Date()); // Timestamp
+    rowData.push(e.parameter.name);
+    rowData.push(e.parameter.email);
+    rowData.push(e.parameter.phone);
+    
+    sheet.getRange(newRow, 1, 1, rowData.length).setValues([rowData]);
+    
+    return ContentService.createTextOutput('Success').setMimeType(ContentService.MimeType.TEXT);
+  }
+  
